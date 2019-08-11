ffdd1bb84dbd5d931717e36beab6e7beaa15b7f3
Gary de Beer <garyd@Garys-MacBook-Pro.local>
2019-08-11 22:25:45 +0200

    add pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'signup', component: SignupComponent },
+  { path: '', redirectTo: '/', pathMatch: 'full' },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-  declarations: [SignupComponent]
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
+  exports: [
+    RouterModule
+  ],
+  declarations: [
+  ]
 })
 export class AppRoutingModule { }
