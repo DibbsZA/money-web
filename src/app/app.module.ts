d607501bdffae8cdb22b7845af43357107020c6d
Gary de Beer <garyd@Garys-MacBook-Pro.local>
2019-08-11 22:26:36 +0200

    add services and models

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,51 @@
+
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { ConnectionService } from './services/connection.service';
+import { CredentialDefinitionsService } from './services/credential-definitions.service';
+import { CredentialsService } from './services/credentials.service';
+import { MessageService } from './services/message.service';
+import { ProofsService } from './services/proofs.service';
+import { SchemaService } from './services/schema.service';
+import { SignupComponent } from './pages/signup/signup.component';
+import { Configuration } from './configuration';
+import { ButtonsModule } from 'ngx-bootstrap/buttons';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignupComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    ButtonsModule.forRoot()
   ],
   providers: [
-
+    ConnectionService,
+    CredentialDefinitionsService,
+    CredentialsService,
+    MessageService,
+    ProofsService,
+    SchemaService
   ],
   bootstrap: [
     AppComponent
   ]
 })
-export class AppModule { }
+export class AppModule {
+  public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders {
+    return {
+      ngModule: AppModule,
+      providers: [{ provide: Configuration, useFactory: configurationFactory }]
+    };
+  }
+}
