c506b1f026dd97d11c8d99de59bf0b159725b8d6
Gary de Beer <garyd@Garys-MacBook-Pro.local>
2019-08-11 22:27:47 +0200

    neaten

--- src/app/services/connection.service.ts
+++ src/app/services/connection.service.ts
@@ -24,7 +24,11 @@ export class ConnectionService {
   public defaultHeaders = new HttpHeaders();
   public configuration = new Configuration();
 
-  constructor(protected httpClient: HttpClient, @Optional() @Inject(BASE_PATH) basePath: string, @Optional() configuration: Configuration) {
+  constructor(
+    protected httpClient: HttpClient,
+    @Optional() @Inject(BASE_PATH) basePath: string,
+    @Optional() configuration: Configuration
+  ) {
     if (basePath) {
       this.basePath = basePath;
     }
