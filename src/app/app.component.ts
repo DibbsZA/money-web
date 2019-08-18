1bf63c4f1560e2edb1ece22f634df1f573842d65
Gary de Beer <garyd@Garys-MacBook-Pro.local>
2019-08-19 01:34:16 +0200

    cleanup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { SchemaService } from './services/schema.service';
-import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
 
 
 @Component({
@@ -12,23 +9,8 @@ import { Observable } from 'rxjs';
 export class AppComponent {
   title = 'money-web';
 
-  schemaData: Observable<any>;
-  mydate;
-  surname = new FormControl('');
-  names = new FormControl('');
-  sex = new FormControl('');
-  nationality = new FormControl('');
-  status = new FormControl('');
-  email = new FormControl('');
-
   constructor(
-    private schemaSvc: SchemaService,
   ) {
-    this.mydate = Date.now();
   }
 
-
-  public getSchema() {
-    this.schemaData = this.schemaSvc.apiSchemasGet();
-  }
 }
