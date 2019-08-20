73ad725d11933a975d11df449c0dc7d7167c72b0
Gary De Beer <garyd@bankservafrica.com>
2019-08-20 16:24:07 +0200

    enhance account proof model

--- src/app/models/account-proof.model.ts
+++ src/app/models/account-proof.model.ts
@@ -5,7 +5,33 @@
  * @date 2019-08-19
  */
 export interface AccountProof {
+  /**
+   * Full Bank Name
+   */
+  bank?: string;
+  /**
+   * Account Holder Initals and Name (Upper Case)
+   * @example H.M. ACCOUNT
+   */
+  accountHolder?: string;
+  /**
+   * Typ of transactional account: CHEQUE, SAVINGS, TRANSMISSION, ?
+   * @example CHEQUE
+   */
+  accountType?: string;
+  /**
+   * Account Number (up to 13 Characters in SA)
+   */
   accountNo?: string;
+  /**
+   * Branch Routing number
+   *
+   */
   branchCode?: string;
-  openDate?: Date;
+  /**
+   * Date on which the account was opened in string format
+   * @example '20190921'
+   */
+  openDate?: string;
+
 }
