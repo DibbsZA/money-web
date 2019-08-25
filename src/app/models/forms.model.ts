ad83a12bafae2f96c1bb90542e2523d7575217be
Gary de Beer <garyd@Garys-MacBook-Pro.local>
2019-08-26 00:38:41 +0200

    interface of signup form data

---
+++ src/app/models/forms.model.ts
@@ -0,0 +1,16 @@
+export interface SignupForm {
+
+  surname?: string;
+  names?: string;
+  sex?: string;
+  nationality?: string;
+  identityNumber?: string;
+  dateOfBirth?: string;
+  countryOfBirth?: string;
+  status?: string;
+  fullAddress?: string;
+  email?: string;
+  cellphone?: string;
+  title?: string;
+  connectionId?: string;
+}
